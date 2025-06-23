df6b51cfd1f84d204a23143b2cded9148ae8fd13
yuyu <sishichulu@gmail.com>
2025-06-23 16:37:49 +0300

    feat: add navbar and styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,20 @@
-import "./App.css";
+import "./css/App.css";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Favorites from "./pages/Favorites";
+import NavBar from "./components/Navbar";
 
 function App() {
   return (
-    <main className="main-content">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/favorites" element={<Favorites />} />
-      </Routes>
-    </main>
+    <div>
+      <NavBar />
+      <main className="main-content">
+        <Routes>
+          <Route path="/movie-search/" element={<Home />} />
+          <Route path="/movie-search/favorites" element={<Favorites />} />
+        </Routes>
+      </main>
+    </div>
   );
 }
 
