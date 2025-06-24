823c4bd92039745e90a4045c175ee0af664cfc00
yuyu <sishichulu@gmail.com>
2025-06-24 23:15:20 +0300

    feat: add genres

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,5 +1,6 @@
 import "../css/MovieCard.css";
 import { useMovieContext } from "../contexts/MovieContext";
+import { GENRES } from "../services/genres";
 
 function MovieCard({ movie }) {
   const { isFavorite, addToFavorites, removeFromFavorites } = useMovieContext();
@@ -10,6 +11,12 @@ function MovieCard({ movie }) {
     if (favorite) removeFromFavorites(movie.id);
     else addToFavorites(movie);
   }
+
+  const genreNames = movie.genre_ids
+    ? movie.genre_ids
+        .map((id) => GENRES.find((g) => g.id === id)?.name)
+        .filter(Boolean)
+    : [];
 
   return (
     <div className="movie-card">
@@ -30,6 +37,15 @@ function MovieCard({ movie }) {
       <div className="movie-info">
         <h3>{movie.title}</h3>
         <p>{movie.release_date?.split("-")[0]}</p>
+        {genreNames.length > 0 && (
+          <div className="movie-genres">
+            {genreNames.map((name) => (
+              <span className="movie-genre-tag" key={name}>
+                {name}
+              </span>
+            ))}
+          </div>
+        )}
       </div>
     </div>
   );
