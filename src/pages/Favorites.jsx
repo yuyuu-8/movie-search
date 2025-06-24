c9e6c34267867e2b4a0ebdaea4a8dd3d688d47d4
yuyu <sishichulu@gmail.com>
2025-06-24 22:33:15 +0300

    fix: fix favorites page

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -5,7 +5,7 @@ import MovieCard from "../components/MovieCard";
 function Favorites() {
   const { favorites } = useMovieContext();
 
-  if (favorites) {
+  if (favorites && favorites.length > 0) {
     return (
       <div className="favorites">
         <h2>Your Favorites</h2>
